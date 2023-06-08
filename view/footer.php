39db7a2e75d93dfe89f56b604cc31f8093b07b0c
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2023-06-08 09:07:03 +0200

    End of integration - 08.06.2023 at 09:02

---
+++ view/footer.php
@@ -0,0 +1,71 @@
+<footer class="ui four column grey message grid" style="padding-bottom: 10px; color: black">
+            <div class="column">
+                <div class="image">
+                    <img src="assets/LOGO MJI.png" alt="" style="width: 50px;">
+                </div>
+                <div class="ui left aligned container">
+                © 2023 Copyright: bxbsmart.com
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="header">
+                    Magasin
+                </div>
+                <div class="row description">
+                    <div>
+                        <a href="">A propos</a>
+                    </div>
+                    <div>
+                        <a href="">Trouver un magasin</a>
+                    </div>
+                    <div>
+                        <a href="">Blogs</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="header">
+                    INFORMATIONS
+                </div>
+                <div class="row description">
+                    <div>
+                        <a href="">Centre d'aide</a>
+                    </div>
+                    <div>
+                        <a href="">Remboursement d'argent</a>
+                    </div>
+                    <div>
+                        <a href="">Informations sur la livraison</a>
+                    </div>
+                    <div>
+                        <a href="">Informations sur la livraison</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="header">
+                    SUPPORT
+                </div>
+                <div class="row description">
+                    <div>
+                        <a href="">Centre d'aide</a>
+                    </div>
+                    <div>
+                        <a href="">Document</a>
+                    </div>
+                    <div>
+                        <a href="">Mes commandes</a>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+
+    </div>
+
+</body>
+
+</html>
---
+++ view/header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Other/html.html to edit this template
+-->
+<html>
+
+<head>
+    <title>BXB - MART</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" type="text/css" href="tpInterface/Semantic-UI/semantic.css">
+
+    <script type="text/javascript" src="tpInterface/Libs/jquery-1.12.4.js"></script>
+    <script type="text/javascript" src="./tpInterface/Semantic-UI/semantic.js"></script>
+
+
+
+</head>
+
+<body>
+    <div class="ui container" style="padding-bottom: 20px; padding-top: 5px;">
+
+        <!-- start navbar -->
+        <div class="ui attached stackable menu">
+            <div class="ui container">
+                <a class="item">
+                    <i class="home icon"></i> Home
+                </a>
+                <a class="item">
+                    <i class="grid layout icon"></i> Browse
+                </a>
+                <a class="item">
+                    <i class="mail icon"></i> Messages
+                </a>
+                <div class="ui simple dropdown item">
+                    Voir plus
+                    <i class="dropdown icon"></i>
+                    <div class="menu">
+                        <a class="item"><i class="edit icon"></i> Profil </a>
+                        <a class="item"><i class="globe icon"></i> Internationalisation</a>
+                        <a class="item"><i class="settings icon"></i> Logout</a>
+                    </div>
+                </div>
+                <div class="ui search right item">
+                    <div class="ui left icon input">
+                        <input class="prompt" type="text" placeholder="Rechercher un produit">
+                        <!-- <i class="github icon"></i> -->
+                        <i class="search icon"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end navbard -->
