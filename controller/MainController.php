5782a945ba228eaf1a1bc0b369218311932066bb
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2023-06-10 02:03:23 +0200

    MainController - commentaires - 09.06.2023 at 02:03

--- controller/MainController.php
+++ controller/MainController.php
@@ -3,34 +3,42 @@
 class MainController
 {
 
+    /* Attribue privée de la bdd */
     private $database;
+
+    /* Le constructeur qui instancie la classe de la bdd */
     public function __construct()
     {
         try {
+            //Objet de la classe bdd
             $this->database = new Database();
         } catch (Exception $e) {
             die($e);
         }
     }
-    
+
+    /* Fx retourner tous les produits */
     public function allProducts()
     {
         //
         return $this->database->allProducts();
     }
 
+    /* Fx retourner un seul produit selon son $id */
     public function product($id)
     {
         //
         return $this->database->product($id);
     }
 
-    public function createProduct($formulaire) {
+    /* Fx pour insérer un produit */
+    public function createProduct($formulaire)
+    {
 
         // var_dump($_FILES);
         $last = $this->database->lastProduct();
         $id = $last['id'] + 1;
-        
+
         $nom = $formulaire["nom"];
         $description = $formulaire["description"];
         $path = self::setMedia($_FILES);
@@ -45,14 +53,12 @@ class MainController
         header("Location: ./dashboard.php");
     }
 
-    public function updateProduct($formulaire) {
-
-        // var_dump($formulaire);
-        // die;
-
+    /* Fx pour modifier un produit */
+    public function updateProduct($formulaire)
+    {
         $last = $this->database->lastProduct();
         $id = $last['id'] + 1;
-        
+
         $nom = $formulaire["nom"];
         $description = $formulaire["description"];
         $couleur = $formulaire["couleur"];
@@ -65,83 +71,86 @@ class MainController
         header("Location: ./dashboard.php");
     }
 
+    /* Fx privée pour la gestion de l'image */
     private function setMedia($path)
     {
-        if($path['image']['error'] == 0)
-        {
+        if ($path['image']['error'] == 0) {
             $pathInfo = pathinfo($_FILES['image']['name']);
             $extension = $pathInfo['extension'];
 
             $extensionAutorisees = array('jpeg', 'jpg', 'JPEG', 'JPG', 'png', 'PNG');
 
-            if(in_array($extension, $extensionAutorisees))
-            {
-              if(file_exists('medias'))
-              {
-                move_uploaded_file($_FILES['image']['tmp_name'], 'medias'.DIRECTORY_SEPARATOR.basename($path['image']['name']));
-                $photoPath = "medias".DS.$path['image']['name'];
+            if (in_array($extension, $extensionAutorisees)) {
+                if (file_exists('medias')) {
+                    move_uploaded_file($_FILES['image']['tmp_name'], 'medias' . DIRECTORY_SEPARATOR . basename($path['image']['name']));
+                    $photoPath = "medias" . DS . $path['image']['name'];
 
-                return $photoPath;
-              }
-              else
-              {
-                mkdir('medias');
-                move_uploaded_file($_FILES['image']['tmp_name'], 'medias'.DIRECTORY_SEPARATOR.basename($path['image']['name']));
-                $photoPath = "medias".DS.$path['image']['name'];
+                    return $photoPath;
+                } else {
+                    mkdir('medias');
+                    move_uploaded_file($_FILES['image']['tmp_name'], 'medias' . DIRECTORY_SEPARATOR . basename($path['image']['name']));
+                    $photoPath = "medias" . DS . $path['image']['name'];
 
-                return $photoPath;
-              }  
-            }
-            else
-            {
+                    return $photoPath;
+                }
+            } else {
                 echo '<script>alert("Les photos uniquement")</script>';
             }
         }
     }
 
-    public function deleteProduct($id) {
+    /* Fx pour supprimer un produit */
+    public function deleteProduct($id)
+    {
         $this->database->deleteProduct($id);
     }
 
 
-    public function login ($email, $password) {
+    /* Fx pour se connecter au système */
+    public function login($email, $password)
+    {
 
         $_SESSION['error'] = false;
 
         $password = sha1($password);
-        if($user = $this->database->login($email, $password)) {
+        if ($user = $this->database->login($email, $password)) {
             // var_dump($user);
             $_SESSION['user'] = [
-                                "username" => $user['name'],
-                                "email" => $user['email'],
-                                "id" => $user['id']
-                                ];
+                "username" => $user['name'],
+                "email" => $user['email'],
+                "id" => $user['id']
+            ];
 
             $_SESSION['error'] = false;
 
             header("Location: ./index.php");
-        }
-        else {
+        } else {
             $_SESSION['error'] = true;
             $_SESSION['email'] = $email;
         }
     }
 
-    public function categories() {
-        return $this->database->categories();
-    }
-
-    public function logout() {
+    /* Fx pour se deconnecter */
+    public function logout()
+    {
         session_destroy();
         header("Location: ./index.php");
     }
 
+    /* Fx pour retourner toutes les catégories */
+    public function categories()
+    {
+        return $this->database->categories();
+    }
+
+    /* FX pour retourner les produits en fx de leur catégorie*/
     public function byCategory($id)
     {
         //
         return $this->database->byCategory($id);
     }
 
+    /* Fx pour retourner une catégorie spécifique */
     public function category($id)
     {
         //
